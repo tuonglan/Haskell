d67b61cf0f2ad50eae7bdf429ab44209473e3f80
Lan Do <tuonglan7777777@gmail.com>
2018-10-05 17:49:53 +0900

    Monad: number_parser with IO

--- Practices/monad/number_parser.hs
+++ Practices/monad/number_parser.hs
@@ -6,6 +6,8 @@
 
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class
 import Control.Applicative
 import qualified Data.Char as C
 
@@ -35,18 +37,34 @@ parseWord _ _ = mzero
 
 -- Parse each word and return the list of all possible outputs 
 parse :: Parsed -> Char -> [Parsed]
-parse p c = (parseHexDigit p c) <|> (parseDigit p c) <|> (parseWord p c)
+parse p c = (parseHexDigit p c) `mplus` (parseDigit p c) `mplus` (parseWord p c)
+
+-- Init the very first elements
+initParsed :: [Parsed]
+initParsed = do
+    (return (Hex 0)) `mplus` (return (Digit 0)) `mplus` (return (Word ""))
+
+foldParseds :: Parsed -> String -> [Parsed]
+foldParseds init s = do
+    foldM parse init s
 
 -- Parse the entire string and retun all possible outcomes 
-parseString :: String -> [Parsed]
+parseStringMaybe :: String -> MaybeT IO [Parsed]
+parseStringMaybe s = do
+    init <- lift $ (return (Hex 0)) `mplus` (return (Digit 0)) `mplus` (return (Word ""))
+    lift $ putStrLn ("The init is: " ++ (show init))
+    return $ foldM parse init s
+
+parseString :: String -> IO [Parsed]
 parseString s = do
-    init <- (return (Hex 0)) `mplus` (return (Digit 0)) `mplus` (return (Word ""))
-    return $ putStrLn "Hello World"
-    foldM parse init s
+    (init:_) <- return $ initParsed
+    putStrLn $ "The init is: " ++ (show init)
+    return $ foldParseds init s
 
 
 main :: IO ()
 main = do
     putStrLn "Please enter a number or string:"
     s <- getLine
-    putStrLn $ show (parseString s)
+    result <- parseString s
+    putStrLn $ show result
